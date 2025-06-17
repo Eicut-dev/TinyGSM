17d93aaa838273f6e29de994252574f22e0997ff
Eicut-dev <>
2025-06-17 11:49:28 +0330

    M66 added

--- src/TinyGsmCommon.h
+++ src/TinyGsmCommon.h
@@ -80,6 +80,16 @@ static void DBG(Args... args) {
 #define DBG(...)
 #endif
 
+// inline void debugLog(const char* format, ...) {
+//     char buffer[128];
+//     va_list args;
+//     va_start(args, format);
+//     vsnprintf(buffer, sizeof(buffer), format, args);
+//     va_end(args);
+
+//     Serial.println(buffer);
+// }
+
 /*
  * CRTP Helper
  */
